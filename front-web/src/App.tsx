8ecbd8863ae81ec3583d77031f1a18a792af99f8
Karina de Oliveira Rovani <kkarinarovani@gmail.com>
2021-01-09 11:51:19 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
+import './App.css';
 import React from 'react';
-import Navbar from "./Navbar"
-import Home from "./Home"
+import Routes from "./Routes"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Routes />
     </>
   );
 }
