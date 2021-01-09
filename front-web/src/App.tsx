65c2696f5314f358eafc3e9c494b3ae564aa53e8
Karina de Oliveira Rovani <kkarinarovani@gmail.com>
2021-01-08 23:08:03 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Counter from './Counter'
+import Navbar from "./Navbar"
 
 function App() {
   return (
     <div>
-      <Counter/>
+      <Navbar />
     </div>
   );
 }
